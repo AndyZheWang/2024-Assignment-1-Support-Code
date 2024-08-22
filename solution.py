8dcf52fc015829f3049c837cdf4c275feaa580c1
ZHEWANGuqANDY <95andyw@gmail.com>
2024-08-22 18:24:06 +1000

    Update solution.py
    1.0

--- solution.py
+++ solution.py
@@ -1,75 +1,135 @@
-import sys
-from constants import *
-from environment import *
+import heapq
+from environment import Environment, widget_get_occupied_cells
 from state import State
-
-"""
-solution.py
-
-This file is a template you should use to implement your solution.
-
-You should implement 
-
-COMP3702 2024 Assignment 1 Support Code
-"""
+from constants import BEE_ACTIONS
 
 
+# StateWrapper class for UCS and A* priority queue management
+class StateWrapper:
+    def __init__(self, state, cost):
+        self.state = state
+        self.cost = cost
+
+    def __lt__(self, other):
+        return self.cost < other.cost
+
+    def __eq__(self, other):
+        return self.state == other.state
+
+    def __hash__(self):
+        return hash(self.state)
+
+
+# Custom StateNode class for managing states and paths
+class StateNode:
+    """
+    Adapted from the COMP3702 Tutorial 3 solution for the BeeBot environment.
+    This class manages state, path, and cost information.
+    """
+
+    def __init__(self, env, state, parent=None, action_from_parent=None, path_steps=0, path_cost=0):
+        self.env = env
+        self.state = state
+        self.parent = parent
+        self.action_from_parent = action_from_parent
+        self.path_steps = path_steps
+        self.path_cost = path_cost
+
+    def get_path(self):
+        """
+        Returns the list of actions from the initial state to this state.
+        """
+        path = []
+        current = self
+        while current.action_from_parent is not None:
+            path.append(current.action_from_parent)
+            current = current.parent
+        path.reverse()
+        return path
+
+    def get_successors(self):
+        """
+        Generates successors from the current state.
+        """
+        successors = []
+        for action in BEE_ACTIONS:
+            success, action_cost, next_state = self.env.perform_action(self.state, action)
+            if success:
+                successors.append(
+                    StateNode(self.env, next_state, self, action, self.path_steps + 1, self.path_cost + action_cost))
+        return successors
+
+    def __lt__(self, other):
+        """
+        Node comparison based on path cost.
+        """
+        return self.path_cost < other.path_cost
+
+
+# Solver class implementing UCS and A* search methods
 class Solver:
-
-    def __init__(self, environment, loop_counter):
+    def __init__(self, environment, lc=None):
         self.environment = environment
-        self.loop_counter = loop_counter
-        #
-        # TODO: Define any class instance variables you require here.
-        # NOTE: avoid performing any computationally expensive heuristic preprocessing operations here - use the preprocess_heuristic method below for this purpose
-        #
+        self.lc = lc
 
-    # === Uniform Cost Search ==========================================================================================
     def solve_ucs(self):
         """
         Find a path which solves the environment using Uniform Cost Search (UCS).
         :return: path (list of actions, where each action is an element of BEE_ACTIONS)
+
+        The core logic of UCS was adapted from the COMP3702 Tutorial 3 solution and translated to the BeeBot environment.
         """
+        frontier = [StateNode(self.environment, self.environment.get_init_state())]
+        heapq.heapify(frontier)
+        visited = {self.environment.get_init_state(): 0}
+        n_expanded = 0
 
-        #
-        #
-        # TODO: Implement your UCS code here
-        #
-        # === Important ================================================================================================
-        # To ensure your code works correctly with tester, you should include the following line of code in your main
-        # search loop:
-        #
-        # self.loop_counter.inc()
-        #
-        # e.g.
-        # while loop_condition(): // (While exploring frontier)
-        #   self.loop_counter.inc()
-        #   ...
-        #
-        # ==============================================================================================================
-        #
-        #
+        while frontier:
+            if self.lc:
+                self.lc.inc()
 
-        pass
+            n_expanded += 1
+            node = heapq.heappop(frontier)
 
-    # === A* Search ====================================================================================================
+            if self.environment.is_solved(node.state):
+                return node.get_path()
 
-    def preprocess_heuristic(self):
+            for s in node.get_successors():
+                if s.state not in visited or s.path_cost < visited[s.state]:
+                    visited[s.state] = s.path_cost
+                    heapq.heappush(frontier, s)
+
+        return None
+
+    def solve_a_star(self):
         """
-        Perform pre-processing (e.g. pre-computing repeatedly used values) necessary for your heuristic,
+        Find a path which solves the environment using A* search.
+        :return: path (list of actions, where each action is an element of BEE_ACTIONS)
+
+        The core logic of A* search was adapted from the COMP3702 Tutorial 3 solution and translated to the BeeBot environment.
         """
+        frontier = [(self.compute_heuristic(self.environment.get_init_state()),
+                     StateNode(self.environment, self.environment.get_init_state()))]
+        heapq.heapify(frontier)
+        visited = {self.environment.get_init_state(): 0}
+        n_expanded = 0
 
-        #
-        #
-        # TODO: (Optional) Implement code for any preprocessing required by your heuristic here (if your heuristic
-        #  requires preprocessing).
-        #
-        # If you choose to implement code here, you should call this method from your solve_a_star method (e.g. once at
-        # the beginning of your search).
-        #
-        #
+        while frontier:
+            if self.lc:
+                self.lc.inc()
 
-        pass
+            n_expanded += 1
+            _, node = heapq.heappop(frontier)
+
+            if self.environment.is_solved(node.state):
+                return node.get_path()
+
+            for s in node.get_successors():
+                if s.state not in visited or s.path_cost < visited[s.state]:
+                    visited[s.state] = s.path_cost
+                    heapq.heappush(frontier, (s.path_cost + self.compute_heuristic(s.state), s))
+
+        return None
 
     def compute_heuristic(self, state):
         """
@@ -77,46 +137,24 @@ class Solver:
         :param state: given state (GameState object)
         :return a real number h(n)
         """
+        uncovered_targets = 0
+        for target in self.environment.target_list:
+            is_covered = False
+            for i in range(self.environment.n_widgets):
+                widget_cells = widget_get_occupied_cells(
+                    self.environment.widget_types[i],
+                    state.widget_centres[i],
+                    state.widget_orients[i]
+                )
+                if target in widget_cells:
+                    is_covered = True
+                    break
+            if not is_covered:
+                uncovered_targets += 1
+        return uncovered_targets
 
-        #
-        #
-        # TODO: Implement your heuristic function for A* search here.
-        #
-        # You should call this method from your solve_a_star method (e.g. every time you need to compute a heuristic
-        # value for a state).
-        #
-
-        pass
-
-    def solve_a_star(self):
+    def preprocess_heuristic(self):
         """
-        Find a path which solves the environment using A* search.
-        :return: path (list of actions, where each action is an element of BEE_ACTIONS)
+        Perform pre-processing (e.g. pre-computing repeatedly used values) necessary for your heuristic,
         """
-
-        #
-        #
-        # TODO: Implement your A* search code here
-        #
-        # === Important ================================================================================================
-        # To ensure your code works correctly with tester, you should include the following line of code in your main
-        # search loop:
-        #
-        # self.loop_counter.inc()
-        #
-        # e.g.
-        # while loop_condition(): //
-        #   self.loop_counter.inc()
-        #   ...
-        #
-        # ==============================================================================================================
-        #
-        #
-
         pass
-
-    #
-    #
-    # TODO: Add any additional methods here
-    #
-    #
